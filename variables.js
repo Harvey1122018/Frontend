0cab2711f6a6466c5403aa2c5dd3c04aecd6591d
Thomas Schirach <thomasschirach@MacBook-Pro-von-Thomas.local>
2022-08-11 17:51:57 +0200

    again

---
+++ variables.js
@@ -0,0 +1,6 @@
+let on="fedor1"
+const data="+"
+console.log(data,on)
+//итог. Типы переменных:
+//null, underfined, string, boolean(true oder false), symbol, number.
+// Лучше задавать через const. Есть еще let и старый var(не используется уже)
